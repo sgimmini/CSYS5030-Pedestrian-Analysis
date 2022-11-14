4796d785336a515d59b23cc7813c8e46af7a7631
Simon Gimmini <Gimmini@stud.uni-heidelberg.de>
2022-11-14 11:24:37 +1100

    Function to convert csv to simple latex table

--- prepare_jidt_data.py
+++ prepare_jidt_data.py
@@ -19,9 +19,37 @@ def add_comment_to_file(file_path):
         else: 
             return False
 
+# function to read a csv file and return a latex table
+def csv_to_latex_table(file_path):
+    with open(file_path, 'r') as f:
+        lines = f.readlines()
+        # create a string that contains the latex table
+        table = ""
+        for j, line in enumerate(lines):
+            # remove the last character of the line (it is a newline character)
+            line = line[:-1]
+            # split the line into the columns
+            columns = line.split(";")
+            #print(columns)
+            if not j == 0:
+                # every column has a maximum length of 4 characters
+                for i in range(len(columns)):
+                    #pass
+                    columns[i] = columns[i][:5]
+
+            # add a row to the table
+            table += f"{' & '.join(columns)} \\\\ \\hline \n"
+        # print the table
+        print(table)
+        print(len(columns))
+        return table
+
 
 
 def main():
+
+    # csv_to_latex_table("/Users/simongimmini/Documents/Studium/CSYS5030 Information Theory/assignments/3/pedestrians/TE_top_10.csv")
+
     # file path 
     file_path = "/Users/simongimmini/Documents/Studium/CSYS5030 Information Theory/assignments/3/pedestrians/data/"
 
